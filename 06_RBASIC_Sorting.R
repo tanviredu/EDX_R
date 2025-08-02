f409f66ccf5bffd74131226ff50b8feff2c03c63
tanviredu <tanviredu2018@gmail.com>
2025-08-02 14:55:56 +0600

    added

--- 06_RBASIC_Sorting.R
+++ 06_RBASIC_Sorting.R
@@ -50,6 +50,27 @@ print(x)
 rank(x)
 
 
+#--------------------------------------------------
+library(dslabs)
+data(murders)
+
+# Access the `state` variable and store it in an object called 'states'
+states <- murders$state
+
+# Sort the object alphabetically and redefine the states object
+states <- sort(states)
+
+# Report the first alphabetical value
+states[1]
+
+# Access the population values ​​of the data set and
+# save it to `pop`
+
+
+# Sort the object and save it to the same object
+
+
+# Report the smallest population size in `pop`
 
 
 
