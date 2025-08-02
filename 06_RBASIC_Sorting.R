dae24e9e4fcdbe5904b5b0b8a081b6c6465c85da
tanviredu <tanviredu2018@gmail.com>
2025-08-02 11:11:45 +0600

    added

--- 06_RBASIC_Sorting.R
+++ 06_RBASIC_Sorting.R
@@ -20,4 +20,44 @@ print(index)
 print(x[index])
 #------------------------------------
 
+#------------------------------------
+index <- order(murders$total)
+print(index)
+print(murders$abb)
+print(murders$abb[index])
+names(murders)
+print(murders$region)
+print(murders$region[index])
+#------------------------------------
+
+#-------------------------------------
+# find the max murder state
+
+max_index = which.max(murders$total)
+print(max_index)
+print(murders$abb[max_index])
+
+# find the min murder state
+
+min_index = which.min(murders$total)
+print(min_index)
+print(murders$abb[min_index])
+
+#------------------------------------
+# you need to find the max and min state in crime
+# but you can only use the rank() and sort() function
+
+index <- rank()
+
+
+
+
+
+
+
+
+
+
+
+
 
