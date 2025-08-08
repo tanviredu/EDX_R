76ec7e3e108578bd9b219c6076e6a3bde1c17a23
tanviredu <tanviredu2018@gmail.com>
2025-08-08 22:19:45 +0600

    added

--- 11_RBASIC_Indexing_Full.R
+++ 11_RBASIC_Indexing_Full.R
@@ -64,8 +64,10 @@ murder_rate <- murders$total / murders$population * 100000
 # Save homicide rate values ​​per 100,000 that are lower
 # than 1,`murder_rate < 1`, in the `low` object
 
+low <- murder_rate < 1
 
 
+#low
 ###########
 
 library(dslabs)
@@ -78,7 +80,7 @@ murder_rate <- murders$total/murders$population*100000
 low <- murder_rate < 1
 
 # Determine the `murder_rate` indices associated with values lower than 1
-
+which(low)
 #####
 
 library(dslabs)
@@ -91,7 +93,7 @@ murder_rate <- murders$total/murders$population*100000
 low <- murder_rate < 1
 
 # Names of states with homicide rates lower than 1
-
+murders$state[low]
 
 ####
 library(dslabs)
@@ -99,15 +101,21 @@ data(murders)
 
 # Save the homicide rate per 100,000 for each state, in
 #`murder_rate`
-
+murder_rate <- murders$total / murders$population * 100000
 
 # Save `murder_rate` values ​​< 1 to `low`
 
-
+low <- murder_rate < 1
+low 
 # Create an `ind` vector for states located in the northeast and with rates of
 # homicide lower than 1.
 
+ind <- low & murders$region == "Northeast"  
+
+
 # `ind` state names
+
+murders$state[ind]
 
 ##
 
@@ -117,13 +125,16 @@ data(murders)
 # Save the homicide rate per 100,000 for each state, in
 # `murder_rate`
 
+murder_rate <- murders$total / murders$population * 100000
+
+avg <- mean(murder_rate)
 
 # Calculate the average homicide rate using `mean` and store it in an object
 # called `avg`
 
 # How many states have homicide rates below average? Count them
 # using `sum`
-
+sum(murder_rate < avg)
 ######
 
 library(dslabs)
@@ -131,13 +142,12 @@ data(murders)
 
 # Store the 3 abbreviations of interest in a vector named `abbs`
 # (remember these are character vectors and require quotes)
-
+abbs <- c("AK", "MI", "IA").
 
 # Match the abbs to the `murders$abb` and store them in `ind`
-
-
+ind <- match(abbs,murders$abb)
 # Use `ind` to retrieve the names of the states
-
+murders$state[ind]
 
 ##
 
@@ -148,10 +158,11 @@ data(murders)
 # Save the 5 abbreviations of interest to `abbs`. (remember that they are vectors of
 # characters)
 
+abbs <- c("MA", "ME", "MI", "MO", "MU")
 
 # Use the %in% operator to check if the `abbs` entries are
 # abbreviations in the homicide data frame
-
+abbs %in% murders$abb
 
 ####
 
@@ -165,6 +176,11 @@ abbs <- c("MA", "ME", "MI", "MO", "MU")
 # Use the `which` function and the `!` operator to find out
 # which of the indexed abbreviations are not part of the set
 # of data and store them in `ind`
+
+ind <- which(!abbs %in% murders$abb)
+
+abbs[ind]
+
 
 
 # Show `abbs` entries that are not real abbreviations
